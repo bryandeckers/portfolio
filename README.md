2fc1d0ed232ee8365d575e261814a820e4f4cf20
Bryan Deckers <r0889617@student.thomasmore.be>
2023-04-13 14:44:11 +0200

    delete old portfolio

--- README.md
+++
@@ -1,6 +0,0 @@
-# POP-Project PS 1
-## Portfolio website
-## Bryan Deckers
-## 1 ITF 07
-## R0889617
- 
---
+++ portfolio/pages/projects.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import busma from "../public/images/busma.png";
+
+export default function Projects() {
+  return (
+    <main className="">
+      <h1 className="text-3xl font-semibold mb-20">Projects</h1>
+      <div className="flex flex-col md:flex-row gap-10 md:gap-40">
+        <div className="w-full md:w-2/3">
+          {/* sizing on div */}
+          <Image src={busma} alt="busma" />
+        </div>
+        <div className="w-full md:w-1/3 flex flex-col justify-between">
+          <div>
+            <h2 className="text-xl font-medium mb-5">Project 1</h2>
+            <p>project beschrijving</p>
+          </div>
+          <div>
+            <p className="mb-10">Technische details</p>
+            <Link href="" className="mt-auto ">
+              Link naar project 1
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}

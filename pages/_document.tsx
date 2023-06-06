793c2e6bd6ea4e84e6c11f80f1d002bf45e0b8f6
Bryan Deckers <r0889617@student.thomasmore.be>
2023-06-06 13:35:45 +0200

    Remove dark mode for now

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,14 @@ import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+
 export default function Document() {
   return (
-    <Html lang="nl-BE">
+    <Html lang="nl-BE" >
       <Head />
       <Navbar />
-      <body className='relative bg-white dark:bg-gray-700 min-w-screen mt-[68px] h-[calc(100vh-68px)] w-[100vw] overflow-y-auto overflow-x-hidden snap-y snap-mandatory scroll-smooth'>
+      <body className='bg-white'>
         <Main />
         <NextScript />
       </body>

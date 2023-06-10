1cb7f447d69dda221a28bd37c749b767433e5696
Bryan Deckers <r0889617@student.thomasmore.be>
2023-06-10 12:57:35 +0200

    footer now in white with black text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,23 +4,23 @@ import { FunctionComponent } from "react";
 
 const Footer: FunctionComponent = () => {
   return (
-    <div className="bg-customblack py-10 text-white mt-auto w-full">
+    <div className="py-10 text-customblack mt-auto w-full">
       <div className="container flex flex-col md:flex-row gap-10 md:gap-0 md:justify-between items-center">
-        <p>Bryan Deckers &copy; {new Date().getFullYear()}</p>
+        <p className="text-xl font-semibold">Bryan Deckers &copy; {new Date().getFullYear()}</p>
         <div className="flex gap-5">
           <Link
             href="https://www.linkedin.com/in/bryandeckers/"
             target="_blank"
             rel="noopener"
           >
-            <Icon icon="fa-brands:linkedin" className="text-2xl" />
+            <Icon icon="fa-brands:linkedin" className="text-4xl md:text-2xl" />
           </Link>
           <Link
             href="https://github.com/bryandeckers"
             target="_blank"
             rel="noopener"
           >
-            <Icon icon="fa-brands:github" className="text-2xl" />
+            <Icon icon="fa-brands:github" className="text-4xl md:text-2xl" />
           </Link>
         </div>
       </div>

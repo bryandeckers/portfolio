f731b0515f6f27196d3f0add6bafd6eda4838eac
Bryan Deckers <bryan.deckers1@gmail.com>
2024-12-03 15:16:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
   reactStrictMode: true,
   images: {
     unoptimized: true,
